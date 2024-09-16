66a12a704fbecfd2d1feface215e2516dd4521b2
Al Ev <alexey_e@mail.ru>
2024-09-16 20:35:55 +0300

    13 human stages

--- U-3/human.py
+++ U-3/human.py
@@ -9,5 +9,25 @@
 # переменные знаком + нужно ввести:
 # print(a1, a2, a3, sep='+')
 # Подсказка: последняя стадия развития - Homo sapiens sapiens.
+#  
+#https://github.com/A-l-E-v/PySynergy/blob/main/U-3/human.py
+#
 
 print()
+print("Антропогенез тест")
+print()
+Stage1 = input ('Введите первый вид (подсказка: Homo habilis): ')
+Stage2 = input ('Введите второй вид (подсказка: Homo rudolfensis): ')
+Stage3 = input ('Введите третий вид (подсказка: Homo erectus): ')
+Stage4 = input ('Введите четвёртый вид (подсказка: Homo georgicus): ')
+Stage5 = input ('Введите пятый вид (подсказка: Homo ergaster): ')
+Stage6 = input ('Введите шестой вид (подсказка: Homo antecessor): ')
+Stage7 = input ('Введите седьмой вид (подсказка: Homo cepranensis): ')
+Stage8 = input ('Введите восьмой вид (подсказка: Homo heidelbergensis): ')
+Stage9 = input ('Введите девятый вид (подсказка: Homo rhodesiensis): ')
+Stage10 = input ('Введите десятый вид (подсказка: Homo neanderthalensis): ')
+Stage11 = input ('Введите одиннадцатый вид (подсказка: Homo sapiens sapiens): ')
+Stage12 = input ('Введите двенадцатый вид (подсказка: Homo sapiens idaltu): ')
+Stage13 = input ('Введите тринадцатый вид (подсказка: Homo floresiensis): ')
+
+
