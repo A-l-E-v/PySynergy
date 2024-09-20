91f7b19e9a22ebc44047a65e91ae40d775332747
Al Ev <alexey_e@mail.ru>
2024-09-20 13:56:21 +0300

    evens.py done!

--- U-6/evens.py
+++ U-6/evens.py
@@ -5,3 +5,15 @@
 # https://github.com/A-l-E-v/PySynergy/blob/main/U-6/evens.py
 #
 
+print()
+print('--- Чётные числа ---')
+print()
+
+A=int(input('Введите число А: '))
+B=int(input('Введите число B: '))
+
+for number in range (A,B+1):
+    if number%2==0: 
+        print (str (number),end='')
+        print (' ',end='')
+print()
