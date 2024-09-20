7a14dc0ba993cb710eca3b935b0ed8346a78fdfb
Al Ev <alexey_e@mail.ru>
2024-09-20 08:18:46 +0300

    con_vow.py is done!

--- U-5/con_vow.py
+++ U-5/con_vow.py
@@ -13,12 +13,52 @@ print()
 print('--- Гласные и согласные ---')
 print()
 
-word = input ('Введите слово: ').lower()
+# строка гласных букв
+letters = 'aeiou'
+
+# ввод слова
+word = input ('Введите слово маленькими латинскими буквами: ')
+
+
 print ()
 print('Анализируем слово: ', word)
+print()
 
-vowels = sum(x in 'aeiou' for x in word)
+# считаем сколько гласных
+vowels = sum(x in letters for x in word)
+# значит, допустим, остальные согласные
 consonants = len(word)-vowels
 
-print ('Гласных: ', vowels)
+print ('Гласных:   ', vowels)
 print ('Согласных: ', consonants)
+print()
+
+# считаем a
+a=word.count(letters[0])
+if a!=0:
+    print ('a - ', a)
+else: print ('a - FALSE')
+
+# считаем e
+e=word.count(letters[1])
+if e!=0:
+    print ('e - ', e)
+else: print ('e - FALSE')
+
+# считаем i
+i=word.count(letters[2])
+if i!=0:
+    print ('i - ', i)
+else: print ('i - FALSE')
+
+# считаем o
+o=word.count(letters[3])
+if o!=0:
+    print ('o - ', o)
+else: print ('o - FALSE')
+
+# считаем u
+u=word.count(letters[4])
+if u!=0:
+    print ('u - ', u)
+else: print ('u - FALSE')
