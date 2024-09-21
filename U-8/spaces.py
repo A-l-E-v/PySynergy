f90833cb70991410dca1385d6fbe3d6da1f3a08f
Al Ev <alexey_e@mail.ru>
2024-09-21 20:32:37 +0300

    spaces done!

--- U-8/spaces.py
+++ U-8/spaces.py
@@ -3,4 +3,24 @@
 # преобразовать все идущие подряд пробелы в один. Выведите измененную
 # строку.
 #
+# https://github.com/A-l-E-v/PySynergy/blob/main/U-8/spaces.py
 #
+print ()
+print ('--- Объединение пробелов ---')
+print ()
+
+# ввод строки с повторяющимися пробелами
+text_line = input ('Введите строку с повторяющимися пробелами: ')
+res_line = ''
+
+for position in range(len(text_line)):
+    if text_line[position] !=' ':
+        res_line+=text_line[position]
+    elif text_line[position-1] != ' ':
+        res_line+=' '
+    else:
+        continue
+
+print (res_line)
+        
+
