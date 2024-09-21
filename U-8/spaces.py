f85f044e16a18b09ffa5549592256fb1e8bfc13b
Al Ev <alexey_e@mail.ru>
2024-09-21 20:44:03 +0300

    spaces.py w comments

--- U-8/spaces.py
+++ U-8/spaces.py
@@ -14,13 +14,16 @@ text_line = input ('Введите строку с повторяющимися 
 res_line = ''
 
 for position in range(len(text_line)):
+    # если это буква, то копируем её
     if text_line[position] !=' ':
         res_line+=text_line[position]
+    # если это пробел, но до этого была буква, то делаем одиночный пробел
     elif text_line[position-1] != ' ':
         res_line+=' '
     else:
+        # при дублировании пробелов не делаем ничего
         continue
 
-print (res_line)
+print ('Строка без лишних пробелов: ', res_line)
         
 
