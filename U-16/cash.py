be1c472860c8320ca28dbd444c36eb7de4ab05e6
Al Ev <alexey_e@mail.ru>
2024-09-28 17:59:01 +0300

    cash class and initial balance

--- U-16/cash.py
+++ U-16/cash.py
@@ -6,7 +6,34 @@
 # ● take_away(X) - забрать X из кассы, либо выкинуть ошибку, что не
 # достаточно денег
 # 
-# 
+# https://github.com/A-l-E-v/PySynergy/blob/main/U-16/cash.py
 #
 
 
+class Kassa():
+
+# инициализация начального баланса
+    def __init__ (self, amount):
+        self.amount = amount
+
+# пополнение на X 
+    def top_up(self,X):
+        self.amount += X
+
+# пересчёт тысяч в кассе
+    def count_1000(self):
+        return self.amount//1000
+
+# выдача суммы X
+    def take_away(self,X):
+        if self.amount >= X:
+            self.amount -=X
+        else:
+            return False
+
+
+Kassa1 = Kassa (150000)
+Kassa2 = Kassa (7500000)
+
+print (f'В кассе 1 сейчас {Kassa1.count_1000()} тысяч.')
+print (f'В кассе 2 сейчас {Kassa2.count_1000()} тысяч.')
